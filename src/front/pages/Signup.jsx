c08058ed8b065319c5c0ceb4dddfc8eca850b5ef
Bryan Navarro <diazbnavarro@gmail.com>
2026-02-05 03:10:54 +0000

    added response messages from backend to signup process

--- src/front/pages/Signup.jsx
+++ src/front/pages/Signup.jsx
@@ -1,19 +1,45 @@
 import { Link } from "react-router-dom";
 import React, {useState} from "react";
-
+import { useNavigate } from "react-router-dom";
 
 
 
 export const Signup = () => {
+    const navigate=useNavigate()
 
     const [email, setEmail]=useState('')
     const [password, setPassword]=useState('')
     const [username, setUsername]=useState('')
 
+    const handleSubmit = async (e) =>{
+        e.preventDefault()
+
+        try {
+            const backendURL = import.meta.env.VITE_BACKEND_URL;
+            const response = await fetch(`${backendURL}api/signup`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({email, password, username})
+            })
+            if (!response.ok){
+                const err = await response.json()
+                alert(err.message || 'signup failed')
+                return
+            }
+            alert('SignUp successful, Please Login')
+            navigate('/')
+        } catch (error) {
+            console.error('signup error:', error);
+            alert('SignUp Failed!')
+        }
+    }
+
     return (
         <div className="container text-center">
             <h1>SignUp</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 
                 <div className="row mb-3">
                     <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
@@ -30,15 +56,15 @@ export const Signup = () => {
                 <div className="row mb-3">
                     <label htmlFor="input_username" className="col-sm-2 col-form-label">Username</label>
                     <div className="col-sm-10">
-                        <input type="text" className="form-control" id="input_username" value={email} onChange={(e)=>setUsername(e.target.value)}/>
+                        <input type="text" className="form-control" id="input_username" value={username} onChange={(e)=>setUsername(e.target.value)}/>
                     </div>
                 </div>
-                <button type="submit" className="btn btn-primary">Sign in</button>
+                <button type="submit" className="btn btn-primary">Sign Up</button>
             </form>
 
             {/* A Link component acts as an anchor tag but is used for client-side routing to prevent page reloads. */}
             <Link to="/">
-                <span classNameName="btn btn-primary btn-lg" href="#" role="button">
+                <span className="btn btn-primary btn-lg mt-1" href="#" role="button">
                     Back home
                 </span>
             </Link>
