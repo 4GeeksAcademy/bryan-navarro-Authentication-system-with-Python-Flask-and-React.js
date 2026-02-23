ffaf7e4a3f48eeb84db5c81a79833f9641ed52e8
Bryan Navarro <diazbnavarro@gmail.com>
2026-02-23 23:35:42 +0000

    added signin successfully

--- src/front/pages/SignIn.jsx
+++ src/front/pages/SignIn.jsx
@@ -1,38 +1,56 @@
 import { Link } from "react-router-dom";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const SignIn = () => {
-    const navigate=useNavigate()
+    const navigate = useNavigate()
 
-    const [email, setEmail]=useState('')
-    const [password, setPassword]=useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-    const handleSubmit = async(e)=>{
+    const handleSubmit = async (e) => {
         e.preventDefault()
 
+        try {
+            const backendURL = import.meta.env.VITE_BACKEND_URL;
+            const response = await fetch(`${backendURL}api/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email, password})
+            })
+            if (!response.ok) {
+                const err = await response.json()
+                alert(err.message || 'signin failed')
+                return
+            }
+            alert('Login successful!')
+            navigate('/')
+        } catch (error) {
 
-    }   
+        }
+    }
 
     return (
         <div className="container text-center">
             <form onSubmit={handleSubmit}>
-                
+
                 <div className="row mb-3">
                     <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
                     <div className="col-sm-10">
-                        <input type="email" className="form-control" id="inputEmail3" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                        <input type="email" className="form-control" id="inputEmail3" value={email} onChange={(e) => setEmail(e.target.value)} />
                     </div>
                 </div>
                 <div className="row mb-3">
                     <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
                     <div className="col-sm-10">
-                        <input type="password" className="form-control" id="inputPassword3" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                        <input type="password" className="form-control" id="inputPassword3" value={password} onChange={(e) => setPassword(e.target.value)} />
                     </div>
                 </div>
                 <button type="submit" className="btn btn-primary">Sign in</button>
             </form>
         </div>
-            
+
     );
 };
